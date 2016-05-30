923f34118764e803872c489ca9c88d9e99c8da5d
Willian Alves <wiilliian.alves@gmail.com>
2016-05-30 08:49:10 -0400

    Produto inserir

---
+++ PROJETO_PRINCIPAL/SISPAN/src/br/com/sispan/controller/ProdutoMB.java
@@ -0,0 +1,33 @@
+package br.com.sispan.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.sispan.interfaces.Bean;
+import br.com.sispan.model.Produto;
+
+@RequestScoped
+@ManagedBean
+public class ProdutoMB extends GenericMB {
+
+	private Produto produto = new Produto();
+		
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	@Override
+	protected Bean getBean() {	 
+		return getProduto();
+	}
+
+	@Override
+	protected void setBean(Bean b) {
+		setProduto((Produto) b);
+	}
+
+}
---
+++ PROJETO_PRINCIPAL/SISPAN/src/br/com/sispan/model/Produto.java
@@ -0,0 +1,63 @@
+package br.com.sispan.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.sispan.interfaces.Bean;
+
+@Entity
+@Table
+public class Produto implements Bean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name = "descricao", nullable = false, length = 255, unique = true)
+	private String descricao;
+	
+	@Column(name = "valor", precision = 4, nullable = false)
+	private Float valor = 0.0f;
+	
+	@Column(name = "quantidade", precision = 0, nullable = false)
+	private Integer quantidade = 0;
+	
+	//Metodos de GET e SET...
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Float getValor() {
+		return valor;
+	}
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+}
