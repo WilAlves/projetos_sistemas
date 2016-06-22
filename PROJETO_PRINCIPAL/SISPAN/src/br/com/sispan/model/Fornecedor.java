20798b1321b5dddf96b31a4d4abb1fadad98b48a
Willian Alves <wiilliian.alves@gmail.com>
2016-06-22 15:46:14 -0400

    incluso lista de fornecedor

--- PROJETO_PRINCIPAL/SISPAN/src/br/com/sispan/model/Fornecedor.java
+++ PROJETO_PRINCIPAL/SISPAN/src/br/com/sispan/model/Fornecedor.java
@@ -27,11 +27,59 @@ public class Fornecedor implements Bean {
 	@GeneratedValue
 	private Long id;
 	
+	@Column(name = "nome_fantasia", nullable = false, length = 255, unique = true)
+	private String nome_fantasia;
+	
 	@Column(name = "cnpj", nullable = false, length = 255, unique = true)
 	private String cnpj;
 	
-	@Column(name = "nome_fantasia", nullable = false, length = 255, unique = true)
-	private String nome_fantasia;
+	@Column(name = "inscricao_estadual", nullable = false, length = 255, unique = true)
+	private String inscricao_estadual;
+	
+	@Column(name = "telefone", nullable = false, length = 255, unique = true)
+	private String telefone;
+	
+	@Column(name = "endereco", nullable = false, length = 255, unique = true)
+	private String endereco;
+	
+	@Column(name = "email", nullable = false, length = 255, unique = true)
+	private String email;
+	
+	public String getInscricao_estadual() {
+		return inscricao_estadual;
+	}
+
+	public void setInscricao_estadual(String inscricao_estadual) {
+		this.inscricao_estadual = inscricao_estadual;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
 	
 	@ManyToMany
     @JoinTable(name="supplier_product",
